58b13b50527ec0206a72132f4dad58c6062a206a
Tashreeqlee <tashyct14@gmail.com>
2023-12-06 16:40:08 +0200

    added table,added total amount, added css

--- script/checkout.js
+++ script/checkout.js
@@ -1,24 +1,61 @@
 // javascript for checkout page
-document.querySelector('#copyYear').textContent =
-new Date().getUTCFullYear()
+document.querySelector("#copyYear").textContent = new Date().getUTCFullYear();
 
-let checkoutWrapper = document.querySelector('[checkout-products]')
-let checkoutData = JSON.parse(localStorage.getItem('cart'))
+// let checkoutWrapper = document.querySelector('[checkout-products]')
+// let checkoutData = JSON.parse(localStorage.getItem('cart'))
+
+// function displayData() {
+//     checkoutWrapper.innerHTML = ""
+//     checkoutData.forEach( item=>{
+//         checkoutWrapper.innerHTML += `
+//         <thead>
+//         <tbody>
+//         <tr>
+//             <td class="text-white">${item.name}</td>
+//             <td><img src="${item.image}" class="w-25 img-fluid" alt="${item.name}"</td>
+//             <td class="text-white">R${item.price}.00</td>
+//         </tr>
+//         </thead>
+//         `
+//     })
+// }
+
+// /*
+// <table>
+//     <thead>
+//         <tr>
+//             <th></th>
+//         </tr>
+//     </thead>
+//     <tbody>
+//         <tr>
+//             <td></td>
+//         </tr>
+//     </tbody>
+// </table>
+// */
+// displayData()
+
+let checkoutWrapper = document.querySelector("[checkout-products]");
+let checkoutData = JSON.parse(localStorage.getItem("cart"));
 
 function displayData() {
-    checkoutWrapper.innerHTML = ""
-    checkoutData.forEach( item=>{
-        checkoutWrapper.innerHTML = `
+  checkoutWrapper.innerHTML = "";
+  let totalAmount = 0;
+  checkoutData.forEach((item) => {
+    checkoutWrapper.innerHTML += `
         <thead>
         <tbody>
         <tr>
-        <th scope="row">1</th>
-        <td class="text-white">${item.name}</td>
-        <td><img src="${item.image}" class="w-25 img-fluid" alt="${item.name}"</td>
-        <td class="text-white">R${item.price}.00</td>
+            <td class="text-white">${item.name}</td>
+            <td><img src="${item.image}" class="w-25 img-fluid" alt="${item.name}"</td>
+            <td class="text-white">R${item.price}.00</td>
         </tr>
         </thead>
-        `
-    })
+        `;
+    totalAmount += +item.price;
+  });
+  document.getElementById("totalAmount").textContent = `R${totalAmount}.00`;
 }
-displayData()
+
+displayData();
