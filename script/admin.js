b792f04c2a09b3aae7696afba7bc4294eb718e43
Tashreeqlee <146819122+Tashreeqlee@users.noreply.github.com>
2023-12-06 21:20:25 +0200

    Contact Page Complete

--- script/admin.js
+++ script/admin.js
@@ -1,3 +1,16 @@
 // javascript for admin page
 document.querySelector('#copyYear').textContent =
 new Date().getUTCFullYear()
+
+let modal = new bootstrap.Modal(document.querySelector('.modal')); // Initialize the modal
+let addProducts=document.querySelector('addProducts')
+addProducts.addEventListener('click',function () {
+  let inputP=document.querySelector('[data-inputP]')
+  let inputN=document.querySelector('[data-inputN]')
+  let inputpR=document.querySelector('[data-inputpR]')
+  let inputI=document.querySelector('[data-inputI]')
+  let newObj= new displayProducts(inputP,inputN,inputpR,inputI)
+  alert('hi')
+  products.push[newObj]
+  modal.show();
+})
--- script/checkout.js
+++ script/checkout.js
@@ -36,6 +36,7 @@ document.querySelector("#copyYear").textContent = new Date().getUTCFullYear();
 // */
 // displayData()
 
+//display data and total amount add up
 let checkoutWrapper = document.querySelector("[checkout-products]");
 let checkoutData = JSON.parse(localStorage.getItem("cart"));
 
@@ -45,11 +46,12 @@ function displayData() {
   checkoutData.forEach((item) => {
     checkoutWrapper.innerHTML += `
         <thead>
-        <tbody>
         <tr>
             <td class="text-white">${item.name}</td>
             <td><img src="${item.image}" class="w-25 img-fluid" alt="${item.name}"</td>
             <td class="text-white">R${item.price}.00</td>
+            <td></td>
+            <td><button data-delete">Delete</button></td>
         </tr>
         </thead>
         `;
@@ -59,3 +61,22 @@ function displayData() {
 }
 
 displayData();
+
+
+// displayData.addEventListener('click',function (event) {
+//   if (event.target.hasAttribute('data-delete')) {
+//       alert('button pressed')
+//       add(event.target.value)
+//   }
+// })
+
+// let deleteButtons = document.querySelectorAll("[data-delete]");
+
+// deleteButtons.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     let productName = event.target.parentNode.parentNode.firstChild.textContent;
+//     let updatedData = checkoutData.filter((item) => item.name !== productName);
+//     localStorage.setItem("cart", JSON.stringify(updatedData));
+//     displayData();
+//   });
+// });
